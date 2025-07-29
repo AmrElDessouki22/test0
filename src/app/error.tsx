f33a58863d58d6c0d4f989a4d0b043565947b48f
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 12:40:00 +0300

    Add src/app/error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+
+class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): { hasError: boolean } {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
